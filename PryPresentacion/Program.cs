fb677f66e9b20114c6bd635086265247821cb025
Juan Sebastian Jimenez <juansebtiji@gmail.com>
2024-09-25 18:48:58 -0500

    Login

--- PryPresentacion/Program.cs
+++ PryPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace PryPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
